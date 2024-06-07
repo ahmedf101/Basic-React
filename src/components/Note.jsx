4d31a2dd48cbf19d454e835e545bcff81a121eb2
Ahmed <ahmed@email.com>
2024-06-07 09:13:05 +0100

    Completed

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a Note </h1>
+      <p>This is going to be the text in note</p>
+    </div>
+  );
+}
+
+export default Note;
